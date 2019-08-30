bee7c8f09f0867928df10f3049c89793a7754cbe
romulo <romulosousa27@hotmail.com>
2019-08-30 18:42:36 -0300

    adicinando estilo no header, instalando o gupl cli, estruturando tag no arquivo index.html

--- landing-page/gulpfile.babel.js
+++ landing-page/gulpfile.babel.js
@@ -53,40 +53,40 @@ const igniteServer = () => {
 
 gulp.task('b-html', () => {
   return gulp.src(PATH.htmlSRC.concat('**/*.html'))
-    .pipe(gulp.dest(PATH.buildDir.concat('/')))
-    .pipe(livereload())
+      .pipe(gulp.dest(PATH.buildDir.concat('/')))
+      .pipe(livereload())
 });
 
 gulp.task('b-css', () => {
   return gulp.src(PATH.sassSRC.concat('**/*.scss'))
-    .pipe(sass({
-      includePaths: require('node-neat').includePaths,
-      style: 'nested',
-      onError: () => {
-        console.log('Warning: Something happens when compile SASS');
-      }
-    }))
-    .pipe(plumbler({ errorHandler: onError() }))
-    .pipe(gulp.dest(PATH.buildDir.concat('/css')))
-    .pipe(livereload())
+      .pipe(sass({
+        includePaths: require('node-neat').includePaths,
+        style: 'nested',
+        onError: () => {
+          console.log('Warning: Something happens when compile SASS');
+        }
+      }))
+      .pipe(plumbler({errorHandler: onError()}))
+      .pipe(gulp.dest(PATH.buildDir.concat('/css')))
+      .pipe(livereload())
 });
 
 gulp.task('b-js', () => {
   return gulp.src(PATH.jsSRC.concat('*.js'))
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(plumbler({ errorHandler: onError }))
-    .pipe(changed(PATH.buildDir.concat('/js')))
-    .pipe(gulp.dest(PATH.buildDir.concat('/js')))
-    .pipe(livereload())
+      .pipe(babel({
+        presets: ['es2015']
+      }))
+      .pipe(plumbler({errorHandler: onError}))
+      .pipe(changed(PATH.buildDir.concat('/js')))
+      .pipe(gulp.dest(PATH.buildDir.concat('/js')))
+      .pipe(livereload())
 });
 
 gulp.task('b-img', () => {
   return gulp.src(PATH.imgSRC.concat('**/*.+(png|jpg|jpeg|gif|svg)'))
-    .pipe(changed(PATH.buildDir.concat('/images')))
-    .pipe(gulp.dest(PATH.buildDir.concat('/images')))
-    .pipe(livereload())
+      .pipe(changed(PATH.buildDir.concat('/images')))
+      .pipe(gulp.dest(PATH.buildDir.concat('/images')))
+      .pipe(livereload())
 });
 
 gulp.task('watch', () => {
@@ -100,7 +100,7 @@ gulp.task('build', ['b-html', 'b-css', 'b-js', 'b-img'], () => {
   return igniteServer();
 });
 
-const ENV = process.env.SERVER_ENV || 'development';
-if (ENV === 'development') {
+const ENV = process.env.SERVER_ENV||'development';
+if(ENV==='development') {
   gulp.task('default', ['build, watch'])
 }
