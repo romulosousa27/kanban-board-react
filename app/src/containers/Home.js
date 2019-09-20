38fd9ab668997410cebdca27a03b0e6bae86570c
Romulo <romulosousa27@hotmail.com>
2019-09-19 22:28:49 -0300

    Componente container Home

---
+++ app/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+        <div>
+          <h1>Kanban Board React.js</h1>
+        </div>
+    );
+  }
+}
