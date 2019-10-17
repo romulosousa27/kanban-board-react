73e35ff6e61fc29680f28bb918ea996ef2396513
romulo <romulosousa@fabricainfo.com>
2019-10-17 18:38:13 -0300

    importando o compoent panel

--- app/src/containers/Home.js
+++ app/src/containers/Home.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-
 import Tilist from './../images/tilst.png';
-
 import './Home.scss';
+import Panel from "../components/Panel";
 
 export default class Home extends Component {
   constructor(props){
@@ -12,25 +11,7 @@ export default class Home extends Component {
   render(){
     return (
         <div>
-          <nav className="navbar navbar-default navbar-fixed-top" id="navbar-center">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar">
-                  <span className="icon-bar"> </span>
-                  <span className="icon-bar"> </span>
-                  <span className="icon-bar"> </span>
-                  <a href="" className="navbar-brand">
-                    <img src={ Tilist } className="img-responsive" alt=""/>
-                  </a>
-                </button>
-              </div>
-              <div className="navbar-collapse collapse" id="navbar">
-                <ul className="nav navbar-nav">
-
-                </ul>
-              </div>
-            </div>
-          </nav>
+          <Panel />
         </div>
     );
   }
