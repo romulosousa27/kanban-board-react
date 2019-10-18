a861197eae1bfb96b53496bf2fcc4d292e85282d
romulo <romulosousa@fabricainfo.com>
2019-10-18 16:48:25 -0300

    adicionando mas um route nos redurcers

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,8 +1,10 @@
-import { combineReducers } from "redux";
-import { connectRouter } from 'connected-react-router'
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+import CardReducer from "./CardReducer";
 
 const createRootReducer = (history) => combineReducers({
   router: connectRouter(history),
+  cards: CardReducer()
 });
 
 export default createRootReducer
