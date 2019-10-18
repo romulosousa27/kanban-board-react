765447bb42733156c6d038e04680cbecf7a43138
romulo <romulosousa@fabricainfo.com>
2019-10-18 16:49:26 -0300

    estrutura quando criar um card

--- app/src/components/Card.js
+++ app/src/components/Card.js
@@ -2,20 +2,15 @@ import React, { Component } from 'react';
 
 
 export default class Card extends Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor(props){
+    super(props)
+  }
 
-	render() {
-		return (
-			<div>
-				<li className="col-xs-12">
-					Task One
-            </li>
-				<li className="col-xs-12">
-					Task Two
-            </li>
-			</div>
-		);
-	}
+  render(){
+    return (
+        <div>
+          <li className="col-xs-12">Task One</li>
+        </div>
+    );
+  }
 }
