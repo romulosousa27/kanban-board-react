0d9f7d2f6d49c5c16d199fe13269d7763a69ed6d
romulo <romulosousa@fabricainfo.com>
2019-11-04 18:56:58 -0300

    ajustando a imagem da navabar que estava fora do contexto das tags

--- app/src/index.js
+++ app/src/index.js
@@ -18,13 +18,13 @@ ReactDOM.render(
       <nav className="navbar navbar-default navbar-fixed-top" id="navbar-center">
         <div className="container-fluid">
           <div className="navbar-header">
+            <a href="#" className="navbar-brand">
+              <img src={Tilist} className="" alt="Kanban React Trello" />
+            </a>
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar">
               <span className="icon-bar"> </span>
               <span className="icon-bar"> </span>
               <span className="icon-bar"> </span>
-              <a href="" className="navbar-brand">
-                <img src={Tilist} className="img-responsive" alt="" />
-              </a>
             </button>
           </div>
           <div className="navbar-collapse collapse" id="navbar">
