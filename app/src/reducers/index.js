bbb021e7727dfe0562d7a627340b0698adde14f7
romulo <romulosousa@fabricainfo.com>
2019-11-04 11:10:49 -0300

    passando o create card como propriedade.

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
-import CardReducer from "./CardReducer";
+import CardReducer from './CardReducer';
 
 const createRootReducer = (history) => combineReducers({
   router: connectRouter(history),
-  cards: CardReducer()
+  cards: CardReducer
 });
 
 export default createRootReducer
