d28e47fe562b84625604972130f27b3abc073ee0
Romulo <romulosousa27@hotmail.com>
2019-11-05 23:38:10 -0300

    implementando o caso de editar.

--- app/src/reducers/CardReducer.js
+++ app/src/reducers/CardReducer.js
@@ -3,13 +3,17 @@ import * as ActionsTypes from './../constants/ActionsTypes';
 export default function cards(state = [], action){
   switch(action.type) {
     case ActionsTypes.CREATE_CARD:
-      console.log(...state, action.payload);
-      return [
-        ...state,
-        action.payload
-      ];
-      // return state.concat(action.payload);
-      break;
+      return [...state, action.payload];
+
+    case ActionsTypes.EDIT_CARD:
+      return state.map(card => {
+        const {id} = action.payload;
+
+        if(id===card.id) {
+          return Object.assign({}, card, action.payload)
+        }
+        return card;
+      });
 
     default:
       return state;
