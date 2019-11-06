b09238a059af8080336687591f285ba4fc65a019
Romulo <romulosousa27@hotmail.com>
2019-11-05 23:40:50 -0300

    Handle de Editar o texto da tarefa criada.

--- app/src/components/EditCard.js
+++ app/src/components/EditCard.js
@@ -3,6 +3,14 @@ import React, { Component } from 'react';
 export default class EditCard extends Component {
   constructor(props){
     super(props)
+
+    this.handleToEdit = this.handleToEdit.bind(this)
+  }
+
+  // Editando o Componente de Card
+  handleToEdit(){
+    const {id} = this.props;
+    this.props.ToEdit(id);
   }
 
   renderEdit(){
@@ -18,7 +26,7 @@ export default class EditCard extends Component {
     return (
         <div>
           <div className="col-xs-10">
-            <input type="text" className="form-control" defaultValue={ this.props.text } readOnly/>
+            <input type="text" className="form-control" defaultValue={ this.props.text } onClick={this.handleToEdit} readOnly/>
           </div>
         </div>
     )
