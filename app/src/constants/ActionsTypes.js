ca9f1ca0db6136ad638a5cfbc1066d15b01d3b1d
romulo <romulosousa@fabricainfo.com>
2019-12-05 18:46:00 -0300

    constantes de ações do panel

--- app/src/constants/ActionsTypes.js
+++ app/src/constants/ActionsTypes.js
@@ -1,7 +1,9 @@
-// Actions para o CARD (Tarefa)
+/** Actions para o CARD (Tarefa) */
 export const CREATE_CARD = 'CREATE_CARD';
 export const EDIT_CARD = 'EDIT_CARD';
 export const DELETE_CARD = 'DELETE_CARD';
 
-// Actions para o Panel (Painel das tarefas)
+/** Actions para o Panel (Painel das tarefas) */
 export const CREATE_PANEL = 'CREATE_PANEL';
+export const EDIT_PANEL = 'CREATE_PANEL';
+export const DELETE_PANEL = 'CREATE_PANEL';
