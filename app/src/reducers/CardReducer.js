750084659388cf8260dd66eb09b0dc19d08697db
romulo <romulosousa@fabricainfo.com>
2019-12-05 18:47:09 -0300

    reducers actions

--- app/src/reducers/CardReducer.js
+++ app/src/reducers/CardReducer.js
@@ -4,27 +4,24 @@ import * as ActionsTypes from './../constants/ActionsTypes';
  * @param state
  * @param action
  */
-export default function cards(state = [], action){
-  switch(action.type) {
+export default function cards(state = [], action) {
+  switch (action.type) {
     case ActionsTypes.CREATE_CARD:
       return [...state, action.payload];
-      break;
 
     case ActionsTypes.EDIT_CARD:
       return state.map(card => {
-        const {id} = action.payload;
+        const { id } = action.payload;
 
-        if(id===card.id) {
+        if (id === card.id) {
           return Object.assign({}, card, action.payload)
         }
         return card;
       });
-      break;
 
     case ActionsTypes.DELETE_CARD:
-      const {id} = action.payload;
-      return state.filter(card => id!==card.id);
-      break;
+      const { id } = action.payload;
+      return state.filter(card => id !== card.id);
 
     default:
       return state;
--- app/src/reducers/PanelReducer.js
+++ app/src/reducers/PanelReducer.js
@@ -5,11 +5,21 @@ import * as ActionsTypes from './../constants/ActionsTypes';
  * @param state
  * @param action
  */
-export default function panels(state = [], action){
-  switch(action.type) {
+export default function panels(state = [], action) {
+  switch (action.type) {
     case ActionsTypes.CREATE_PANEL:
-      return [ ...state, action.payload];
-      break;
+      return [...state, action.payload];
+
+    case ActionsTypes.EDIT_PANEL:
+      return state.map(panel => {
+        const {id} = action.payload;
+        
+        if (id === panel.id) {
+          return Object.assign({}, panel, action.payload)
+        }
+
+        return panel;
+      })
 
     default:
       return state;
