bceb05fca7b4a769cfd3dda7c000ad7ccd580708
romulo <romulosousa@fabricainfo.com>
2019-12-06 13:30:26 -0300

    Adicionado açoes de crud

--- app/src/constants/ActionsTypes.js
+++ app/src/constants/ActionsTypes.js
@@ -5,5 +5,11 @@ export const DELETE_CARD = 'DELETE_CARD';
 
 /** Actions para o Panel (Painel das tarefas) */
 export const CREATE_PANEL = 'CREATE_PANEL';
-export const EDIT_PANEL = 'CREATE_PANEL';
-export const DELETE_PANEL = 'CREATE_PANEL';
+export const EDIT_PANEL = 'EDIT_PANEL';
+export const DELETE_PANEL = 'DELETE_PANEL';
+
+/** Transição de Panels e Cards */
+export const MOVE_PANEL   = 'MOVE_PANEL';
+export const MOVE_CARD    = 'MOVE_CARD';
+export const INSERT_IN_PANEL = 'INSERT_IN_PANEL';
+export const REMOVE_FROM_PANEL = 'REMOVE_FROM_PANEL';
