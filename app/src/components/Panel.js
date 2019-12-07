a4476128e2985b126bfe30236873e73c99e4ef8f
Romulo <romulosousa27@hotmail.com>
2019-12-07 13:58:15 -0300

    implementando o drag n drop no componente de panel

--- app/src/components/Panel.js
+++ app/src/components/Panel.js
@@ -6,6 +6,7 @@ import Cards from './Cards';
 import Edit from './Edit';
 import { connect } from 'react-redux';
 import * as Types from './../constants/Types';
+import { DragSource, DropTarget } from "react-dnd";
 
 class Panel extends Component {
   static propTypes = {
@@ -42,37 +43,40 @@ class Panel extends Component {
   }
 
   render(){
-    const {cards, panel} = this.props;
-    return (
-        <div className="col-md-3">
-          <div className="panel panel-default">
-            <div className="panel-heading">
-              <Edit
-                  id={ panel.id }
-                  edit={ panel.edit }
-                  text={ panel.text }
-                  ToEdit={ this.props.editPanel }
-                  editComponent={ this.props.editPanel }
-                  deleteComponent={ this.props.deletePanel }
-
-              />
+    const {cards, panel, connectDragPreview, connectDragSource, connectDropTarget} = this.props;
+    return connectDragPreview(
+        connectDropTarget(
+            <div className="col-md-3">
+              { connectDragSource(
+                  <div className="panel panel-default">
+                    <div className="panel-heading">
+                      <Edit
+                          id={ panel.id }
+                          edit={ panel.edit }
+                          text={ panel.text }
+                          ToEdit={ this.props.editPanel }
+                          editComponent={ this.props.editPanel }
+                          deleteComponent={ this.props.deletePanel }
+                      />
+                    </div>
+                    <div className="panel-body">
+                      <Cards
+                          cards={ cards }
+                          ToEdit={ this.props.editCard }
+                          editCard={ this.props.editCard }
+                          deleteCard={ this.props.deleteCard }
+                          moveCard={ this.props.moveCard }
+                      />
+                    </div>
+                    <div className="panel-footer">
+                      <button className="btn btn-primary" onClick={ this.handleCreateCard }>
+                        <i className="ion-plus-round"></i> Card
+                      </button>
+                    </div>
+                  </div>
+              ) }
             </div>
-            <div className="panel-body">
-              <Cards
-                  cards={ cards }
-                  ToEdit={ this.props.editCard }
-                  editCard={ this.props.editCard }
-                  deleteCard={ this.props.deleteCard }
-                  moveCard={ this.props.moveCard }
-              />
-            </div>
-            <div className="panel-footer">
-              <button className="btn btn-primary" onClick={ this.handleCreateCard }>
-                <i className="ion-plus-round"></i> Card
-              </button>
-            </div>
-          </div>
-        </div>
+        )
     );
   }
 }
@@ -107,11 +111,45 @@ const mapDispatchToProps = (dispatch) => {
       }
       return dispatch(PanelActions.removeFromPanel(panelID, cardID));
     },
-    moveCard: (id, monitorID) => dispatch(PanelActions.movePanel(id, monitorID)),
+    movePanel: (id, monitorID) => dispatch(PanelActions.movePanel(id, monitorID)),
     insertInPanel: (id, monitorID) => dispatch(PanelActions.insertInPanel(id, monitorID)),
   }
 };
 
 /** Drag and Drop */
+const dragDropSrc = {
+  beginDrag(props){
+    return {
+      id: props.panel.id
+    }
+  }
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Panel)
+const collect = (connnect, monitor) => ({
+  connectDragSource: connnect.dragSource(),
+  isDragging: monitor.isDragging(),
+  connectDragPreview: connnect.dragPreview()
+});
+
+const collectTarget = (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget()
+});
+
+const panelHoverTarget = {
+  hover(props, monitor){
+    const {id} = props.panel;
+    const monitorProps = monitor.getItem();
+    const monitorType = monitor.getItemType();
+    const monitor_id = monitorProps.id;
+
+    if(id!==monitor_id) {
+      return props.movePanel(id, monitor_id)
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+    DragSource(Types.PANEL, dragDropSrc, collect)(
+        DropTarget(Types.PANEL, panelHoverTarget, collectTarget)(Panel)
+    )
+)
