eff5f9bf31ba34f16f8a7d589599b8aeabdb3de1
Romulo <romulosousa27@hotmail.com>
2019-12-09 21:06:21 -0300

    adicionando espacamento

--- app/src/containers/Home.js
+++ app/src/containers/Home.js
@@ -24,7 +24,7 @@ class Home extends Component {
 
     return (
         <DndProvider backend={HTML5Backend}>
-          <div className="col-xs-12">
+          <div className="col-xs-12 spacing">
             <button className="btn btn-primary" onClick={ this.handleCreatePanel }>
               <i className="ion-plus-round"></i> New Panel
             </button>
