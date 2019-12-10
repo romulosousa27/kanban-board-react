3250c1fb5749bde80401d8f07b568d116fce1066
Romulo <romulosousa27@hotmail.com>
2019-12-09 21:05:59 -0300

    Adcionando o caso de modificar o card do panel

--- app/src/reducers/PanelReducer.js
+++ app/src/reducers/PanelReducer.js
@@ -40,6 +40,64 @@ export default function panels(state = [], action){
         ]
       });
 
+    case ActionsTypes.MOVE_CARD:
+      const targetCard_id = action.payload.id;
+      const monitorCard_id = action.payload.monitor_id;
+
+      let targetPanel = state.filter( panel => panel.cards.indexOf(targetCard_id));
+      let monitorPanel = state.filter( panel => panel.cards.indexOf(monitorCard_id));
+
+      targetPanel = targetPanel[0];
+      monitorPanel = monitorPanel[0];
+
+      const targetCardIndex = targetPanel.cards.indexOf(targetCard_id);
+      const monitorCardIndex = monitorPanel.card.indexOf(monitorCard_id);
+
+      if(targetPanel.id === monitorPanel.id){
+        return state.map((panel) => {
+          const panel_id = panel.id;
+
+          if(monitorPanel.id !== panel_id){
+            return panel;
+          }
+
+          return Object.assign({}, panel,  {
+            cards: update(monitorPanel.cards, {
+              $splice: [
+                  [monitorCardIndex, 1],
+                  [targetCardIndex, 0, monitorCard_id]
+              ]
+            })
+          });
+
+        });
+      }
+
+      return state.map((panel) => {
+        const panel_id = panel.id;
+
+        if(targetPanel.id === panel_id){
+          return Object.assign({}, panel, {
+            cards: update(panels.cards, {
+              $splice: [
+                  [targetCardIndex, 0, monitor_id]
+              ]
+            })
+          });
+        }
+
+        if(monitorPanel.id === panel_id){
+          return Object.assign({}, panel, {
+            cards: update(panels.cards, {
+              $splice: [
+                [monitorCardIndex, 1]
+              ]
+            })
+          });
+        }
+
+      });
+
     default:
       return state;
   }
